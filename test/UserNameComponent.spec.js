d63a8e97caf7085049b5350859d6d966b91acf88
Rija Menage <rija+git@cinecinetique.com>
2019-12-16 09:49:57 +0800

    Remove duplication by abstracting component instantiation into factory
    I just followed the docs:
    
    https://vuejs.org/v2/cookbook/unit-testing-vue-components.html

--- test/UserNameComponent.spec.js
+++ test/UserNameComponent.spec.js
@@ -1,30 +1,34 @@
 import { shallowMount } from '@vue/test-utils'
 import UserNameComponent from '../src/components/UserNameComponent.vue'
 
+const factory = function (values = {}) {
+  return shallowMount(UserNameComponent, {
+    data () {
+      return {
+        ...values
+      }
+    }
+  })
+}
+
 describe('Username component', function () {
 	it('should use prompt for label', function () {
 
-		const wrapper = shallowMount(UserNameComponent, {
-	      data: function () {
-	      	return {
+		const wrapper = factory({
 		        prompt: 'foo bar',
 		        username: '',
-		      }
-		  }
-	    })
+			})
+
 		expect(wrapper.find('.form-label').text()).toContain(wrapper.vm.prompt)
 	})
 
 	it('sets an explanatory prompt', function() {
-		const wrapper = shallowMount(UserNameComponent, {
-	      data: function () {
-	      	return {
+		const wrapper = factory({
 		        prompt: 'foo bar',
 		        username: '',
 		        minLength: 4,
-		      }
-		  }
-	    })
+	    	})
+
 		expect(wrapper.vm.fullPrompt).toBe('foo bar ('+wrapper.vm.minLength+' char. min.):')
 	})
 })

118d94b07a9a699e1c43f963d255b393422bef4f
Rija Menage <rija+git@cinecinetique.com>
2019-12-17 14:30:59 +0800

    Fix coverage report generation
    what was required was to add a coverage entry in the preprocessor for spec.js files
    and make sure @babel/core and @babel/preset-env are installed in addition to babel-* packages.
    
    Finally babel-loader needs to be added in webpack config's rules.

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.spec.js': ['webpack', 'sourcemap']
+      '**/*.spec.js': ['webpack', 'sourcemap','coverage']
     },
 
     webpack: webpackConfig,
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,16 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
